b6ca2c8fcc566b0e40b10d042251d60fd262ea11
Eriktech <erik.lopez.tech@gmail.com>
2020-03-04 19:17:19 -0500

    now config again

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-const withStylus = require('@zeit/next-stylus')
-module.exports = withStylus({
-    target: 'serverless',
-    exportTrailingSlash: true
-  })
+// const withStylus = require('@zeit/next-stylus')
+// module.exports = withStylus({
+//     target: 'serverless',
+//     exportTrailingSlash: true
+//   })
+
+module.exports = {exportTrailingSlash: true}
+
